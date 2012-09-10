b9fbf3641c8671136eeb78728fd2b9d2a700424d
Kevin Scott <kevin@scottlabsllc.com>
2012-09-09 22:44:04 -0400

    try making order

--- static/js/router.js
+++ static/js/router.js
@@ -1,53 +1,51 @@
 var log = function(msg){if (1 && window['console']) {console.log(msg); } }
-define(['jquery'], function ($) {
-    //Paste plugin code here.
-    // Filename: router.js
-    define([
-      'jqueryui',
-      'fancybox',
-      'effects',
-      'underscore',
-      'backbone',
-      'facebook',
-      'paper',
-      'tween',
-      'controllers/profile',
-      'controllers/web',
-      'controllers/hunt'
-    ], function($ui, fancybox, fx, _, Backbone, facebook, paperJS, tweenJS, profileView, webView, huntView){
-      var AppRouter = Backbone.Router.extend({
-        routes: {
-          // Define some URL routes
-          'projects': 'showProjects',
-          'users': 'showUsers',
-          'web' : 'web',
-          'hunt' : 'hunt',
 
-          // Default
-          '*actions': 'profile'
-        },
-        web : function() {
-          log('web view');
-          
-          webView.render();
-        },
-        hunt : function() {
-          log('hunt');
-          huntView.render();
-        },
-        profile: function(actions){
-          profileView.render();
-        }
-      });
+// Filename: router.js
+define([
+  'jquery',
+  'jqueryui',
+  'fancybox',
+  'effects',
+  'underscore',
+  'backbone',
+  'facebook',
+  'paper',
+  'tween',
+  'controllers/profile',
+  'controllers/web',
+  'controllers/hunt'
+], function($, $ui, fancybox, fx, _, Backbone, facebook, paperJS, tweenJS, profileView, webView, huntView){
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      // Define some URL routes
+      'projects': 'showProjects',
+      'users': 'showUsers',
+      'web' : 'web',
+      'hunt' : 'hunt',
 
-      var initialize = function(){
+      // Default
+      '*actions': 'profile'
+    },
+    web : function() {
+      log('web view');
+      
+      webView.render();
+    },
+    hunt : function() {
+      log('hunt');
+      huntView.render();
+    },
+    profile: function(actions){
+      profileView.render();
+    }
+  });
 
-        var app_router = new AppRouter;
-        Backbone.history.start();
-      };
-      return {
-        initialize: initialize
-      };
-    });
+  var initialize = function(){
 
+    var app_router = new AppRouter;
+    Backbone.history.start();
+  };
+  return {
+    initialize: initialize
+  };
 });
