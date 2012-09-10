5c1c3b07a0edfe167605e9af73e42d91b68f3301
Kevin Scott <kevin@scottlabsllc.com>
2012-09-10 00:05:28 -0400

    remove permissions

--- static/js/controllers/profile.js
+++ static/js/controllers/profile.js
@@ -246,12 +246,18 @@ define([
             e.preventDefault();
             FB.login(function(response) 
             {
-                if (! response.authResponse) 
+                if (response.authResponse) 
+                {
+                  
+                    //login success
+                } 
+                else 
                 {
                     alert('User cancelled login or did not fully authorize.');
                 }
+//            }, {scope: 'email,user_likes,user_about_me,friends_about_me,user_activities,friends_activities,user_groups,friends_groups,user_interests,friends_interests,user_likes,friends_likes,user_location,user_questionsfriends_questions,user_religion_politics,friends_religion_politics,user_subscriptions,friends_subscriptionsuser_work_history,friends_work_history'})
 
-            }, {scope: 'email,user_likes,user_about_me,friends_about_me,user_activities,friends_activities,user_groups,friends_groups,user_interests,friends_interests,user_likes,friends_likes,user_location,user_questionsfriends_questions,user_religion_politics,friends_religion_politics,user_subscriptions,friends_subscriptionsuser_work_history,friends_work_history'})
+            }, {scope: 'email,user_likes,user_about_me,friends_about_me,user_activities,friends_activities,user_groups,friends_groups,user_interests,friends_interests,user_likes,friends_likes,user_location'})
           });
           FB.Event.subscribe('auth.login', function(response) {
             $('.fb-login-button').remove();
