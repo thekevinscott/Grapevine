d7e1c121ae16aee176f029086fe6c07d66dac183
Kevin Scott <kevin@scottlabsllc.com>
2012-09-09 23:09:40 -0400

    GODDAMN TYPOS

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,7 @@
 
 require.config({
   paths: {
-  	jquery: 	'libs/jquery/jquery.min'
+  	jquery: 	'libs/jquery/jquery.min',
     underscore: 'libs/underscore/underscore-min',
     backbone:   'libs/backbone/backbone-optamd3-min',
     text:       'libs/require/text',
